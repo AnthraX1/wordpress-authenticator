3f386d021755727e4ecf3358374e79265544b0e5
AnthraX1 <oneshootonekill@gmail.com>
2014-05-25 17:34:04 +1000

    replaced tabs with double spaces

--- prosody/mod_auth_wordpress.lua
+++ prosody/mod_auth_wordpress.lua
@@ -10,35 +10,35 @@ local params = module:get_option("wordpress");
 
 function encode64(input,count)
   local index_table = './0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
-	local output="";
-	local i=0;
-	while i< count do
-		i=i+1
-		local value=string.byte (string.sub(input,i,i));
-		local pos=bit.band(value,0x3f);
-		output =output .. string.sub(index_table,pos+1,pos+1);
-		if i < count then
-			value = bit.bor(value,bit.lshift(string.byte(string.sub(input,i+1,i+1)),8));
-		end	
-		local tmp= bit.band(bit.rshift(value,6),0x3f);
-		output=output  .. string.sub(index_table,tmp+1,tmp+1);
-		i=i+1;
-		if i >= count then
-			break;
-		end	
-		if i<count then
-			value = bit.bor(value,bit.lshift(string.byte(string.sub(input,i+1,i+1)),16));
-		end
-		tmp= bit.band(bit.rshift(value,12),0x3f);
-		output=output .. string.sub(index_table,tmp+1,tmp+1);
-		i=i+1;
-		if i >= count then
-			break;
-		end	
-		tmp= bit.band(bit.rshift(value,18),0x3f);
-		output=output .. string.sub(index_table,tmp+1,tmp+1);
-	end
-	return output;
+  local output="";
+  local i=0;
+  while i< count do
+    i=i+1
+    local value=string.byte (string.sub(input,i,i));
+    local pos=bit.band(value,0x3f);
+    output =output .. string.sub(index_table,pos+1,pos+1);
+    if i < count then
+      value = bit.bor(value,bit.lshift(string.byte(string.sub(input,i+1,i+1)),8));
+    end  
+    local tmp= bit.band(bit.rshift(value,6),0x3f);
+    output=output  .. string.sub(index_table,tmp+1,tmp+1);
+    i=i+1;
+    if i >= count then
+      break;
+    end  
+    if i<count then
+      value = bit.bor(value,bit.lshift(string.byte(string.sub(input,i+1,i+1)),16));
+    end
+    tmp= bit.band(bit.rshift(value,12),0x3f);
+    output=output .. string.sub(index_table,tmp+1,tmp+1);
+    i=i+1;
+    if i >= count then
+      break;
+    end  
+    tmp= bit.band(bit.rshift(value,18),0x3f);
+    output=output .. string.sub(index_table,tmp+1,tmp+1);
+  end
+  return output;
 end
 
 function wp_hash(password,salt)
@@ -132,59 +132,59 @@ end
 
 -- database methods from mod_storage_sql.lua
 local function test_connection()
-	if not connection then return nil; end
-	if connection:ping() then
-		return true;
-	else
-		module:log("debug", "Database connection closed");
-		connection = nil;
-	end
+  if not connection then return nil; end
+  if connection:ping() then
+    return true;
+  else
+    module:log("debug", "Database connection closed");
+    connection = nil;
+  end
 end
 
 local function connect()
-	if not test_connection() then
-		prosody.unlock_globals();
-		local dbh, err = DBI.Connect(
-			"MySQL", params.database,
-			params.username, params.password,
-			params.host, params.port
-		);
-		prosody.lock_globals();
-		if not dbh then
-			module:log("debug", "Database connection failed: %s", tostring(err));
-			return nil, err;
-		end
-		module:log("debug", "Successfully connected to database");
-		dbh:autocommit(false); -- don't commit automatically
-		connection = dbh;
-		return connection;
-	end
+  if not test_connection() then
+    prosody.unlock_globals();
+    local dbh, err = DBI.Connect(
+      "MySQL", params.database,
+      params.username, params.password,
+      params.host, params.port
+    );
+    prosody.lock_globals();
+    if not dbh then
+      module:log("debug", "Database connection failed: %s", tostring(err));
+      return nil, err;
+    end
+    module:log("debug", "Successfully connected to database");
+    dbh:autocommit(false); -- don't commit automatically
+    connection = dbh;
+    return connection;
+  end
 end
 
 do -- process options to get a db connection
-	local ok;
-	prosody.unlock_globals();
-	ok, DBI = pcall(require, "DBI");
-	if not ok then
-		package.loaded["DBI"] = {};
-		module:log("error", "Failed to load the LuaDBI library for accessing SQL databases: %s", DBI);
-		module:log("error", "More information on installing LuaDBI can be found at http://prosody.im/doc/depends#luadbi");
-	end
-	prosody.lock_globals();
-	if not ok or not DBI.Connect then
-		return; -- Halt loading of this module
-	end
+  local ok;
+  prosody.unlock_globals();
+  ok, DBI = pcall(require, "DBI");
+  if not ok then
+    package.loaded["DBI"] = {};
+    module:log("error", "Failed to load the LuaDBI library for accessing SQL databases: %s", DBI);
+    module:log("error", "More information on installing LuaDBI can be found at http://prosody.im/doc/depends#luadbi");
+  end
+  prosody.lock_globals();
+  if not ok or not DBI.Connect then
+    return; -- Halt loading of this module
+  end
 
-	params = params or {};
-	
-	params.host = params.host or "localhost";
-	params.port = params.port or 3306;
-	params.database = params.database or "wordpress";
-	params.username = params.username or "NOTroot";
-	params.password = params.password or "";
-	params.prefix = params.prefix or "wp_";
-	
-	assert(connect());
+  params = params or {};
+  
+  params.host = params.host or "localhost";
+  params.port = params.port or 3306;
+  params.database = params.database or "wordpress";
+  params.username = params.username or "NOTroot";
+  params.password = params.password or "";
+  params.prefix = params.prefix or "wp_";
+  
+  assert(connect());
 end
 
 module:add_item("auth-provider", new_wordpress_provider(module.host));
